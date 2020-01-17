1c6aabff32e3fa09d3a28f092fe352b8b85c276b
Lesley Chang <lesley.chang@thinkpower-info.com>
2020-01-17 23:44:42 +0800

    add e2e job

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -13,7 +13,14 @@ exports.config = {
     './src/**/*.e2e-spec.ts'
   ],
   capabilities: {
-    browserName: 'chrome'
+    browserName: 'chrome',
+    chromeOptions: {   
+      args: [
+        '--headless', 
+        '--no-sandbox', 
+        '--disable-gpu',
+        '--window-size=1920,1080'
+    ]}
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
